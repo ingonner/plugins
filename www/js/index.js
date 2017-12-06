3568133d438e1bcaa42652b0999eccebb9f7d115
OSCARMG\Oscar <ingonner1@gmail.com>
2017-12-05 20:22:24 -0600

    Accelometer

--- www/js/index.js
+++ www/js/index.js
@@ -35,6 +35,8 @@ var app = {
         document.getElementById("imageCapture").addEventListener("click", imageCapture);
         document.getElementById("videoCapture").addEventListener("click", videoCapture);
       
+        document.getElementById("getAcceleration").addEventListener("click", getAcceleration);
+        document.getElementById("watchAcceleration").addEventListener("click", watchAcceleration);
         
 // -------------------------------------------------------------------------------------        
         function createContact() {
@@ -380,10 +382,58 @@ function videoCapture() {
    }
 }
 //------------------------------------------------------------------------         
+function getAcceleration() {
+   navigator.accelerometer.getCurrentAcceleration(
+      accelerometerSuccess, accelerometerError);
 
+   function accelerometerSuccess(acceleration) {
+      alert('Acceleration X: ' + acceleration.x + '\n' +
+         'Acceleration Y: ' + acceleration.y + '\n' +
+         'Acceleration Z: ' + acceleration.z + '\n' +
+         'Timestamp: '      + acceleration.timestamp + '\n');
+   };
+
+   function accelerometerError() {
+      alert('onError!');
+   };
+}
+
+function watchAcceleration() {
+   var accelerometerOptions = {
+      frequency: 3000
+   }
+   var watchID = navigator.accelerometer.watchAcceleration(
+      accelerometerSuccess, accelerometerError, accelerometerOptions);
+
+   function accelerometerSuccess(acceleration) {
+      alert('Acceleration X: ' + acceleration.x + '\n' +
+         'Acceleration Y: ' + acceleration.y + '\n' +
+         'Acceleration Z: ' + acceleration.z + '\n' +
+         'Timestamp: '      + acceleration.timestamp + '\n');
+
+      setTimeout(function() {
+         navigator.accelerometer.clearWatch(watchID);
+      }, 10000);
+   };
+
+   function accelerometerError() {
+      alert('onError!');
+   };
+	
+}
         
 //------------------------------------------------------------------------         
         
+//------------------------------------------------------------------------         
+
+//------------------------------------------------------------------------         
+
+//------------------------------------------------------------------------         
+
+//------------------------------------------------------------------------         
+
+//------------------------------------------------------------------------         
+
         
     },
     // Update DOM on a Received Event
