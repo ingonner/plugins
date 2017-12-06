4fba67d40362b569e694da04bf2dc96b35bd4b56
OSCARMG\Oscar <ingonner1@gmail.com>
2017-12-05 19:03:22 -0600

    Con contacts

--- www/js/index.js
+++ www/js/index.js
@@ -1,39 +1,94 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
+
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+  
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        console.log(navigator.contacts);
+        document.getElementById("createContact").addEventListener("click", createContact);
+        document.getElementById("findContact").addEventListener("click", findContact);
+        document.getElementById("deleteContact").addEventListener("click", deleteContact);
+        
+      
+        
+// -------------------------------------------------------------------------------------        
+        function createContact() {
+   var myContact = navigator.contacts.create({"displayName": "Test User"});
+   myContact.save(contactSuccess, contactError);
+    
+   function contactSuccess() {
+      alert("Contact is saved!");
+   }
+	
+   function contactError(message) {
+      alert('Failed because: ' + message);
+   }
+	
+}
+// -------------------------------------------------------------------------------------        
+    
+    function findContacts() {
+   var options = new ContactFindOptions();
+   options.filter = "";
+   options.multiple = true;
+   fields = ["displayName"];
+   navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+    
+   function contactfindSuccess(contacts) {
+      for (var i = 0; i < contacts.length; i++) {
+         alert("Display Name = " + contacts[i].displayName);
+      }
+   }
+	
+   function contactfindError(message) {
+      alert('Failed because: ' + message);
+   }
+	
+}    
+        
+   // -------------------------------------------------------------------------------------        
+     
+function deleteContact() {
+   var options = new ContactFindOptions();
+   options.filter = "Test User";
+   options.multiple = false;
+   fields = ["displayName"];
+   navigator.contacts.find(fields, contactfindSuccess, contactfindError, options);
+
+   function contactfindSuccess(contacts) {
+      var contact = contacts[0];
+      contact.remove(contactRemoveSuccess, contactRemoveError);
+
+      function contactRemoveSuccess(contact) {
+         alert("Contact Deleted");
+      }
+
+      function contactRemoveError(message) {
+         alert('Failed because: ' + message);
+      }
+   }
+
+   function contactfindError(message) {
+      alert('Failed because: ' + message);
+   }
+	
+}    
+        
+    // -------------------------------------------------------------------------------------        
+   
+        
+        
+        
+        
+        
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
